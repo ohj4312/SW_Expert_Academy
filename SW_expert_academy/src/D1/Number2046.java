cb7e2d38e924886bade9a5c282fc12ab6857cf30
ohj4312 <ohj4312@naver.com>
2021-07-24 23:24:58 +0900

    D1-2046 Solution

---
+++ SW_expert_academy/src/D1/Number2046.java
@@ -0,0 +1,15 @@
+package D1;
+import java.util.Scanner;
+
+
+//스탬프 찍기(2046-D1)
+public class Number2046 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		
+		for(int i=0;i<num;i++) {
+			System.out.print("#");
+		}
+	}
+}
