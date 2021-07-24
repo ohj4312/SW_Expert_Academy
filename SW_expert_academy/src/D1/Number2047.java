3d99f28aa95282a91f51087b646daa4ce2997e4e
ohj4312 <ohj4312@naver.com>
2021-07-24 23:25:12 +0900

    D1-2047 Solution

---
+++ SW_expert_academy/src/D1/Number2047.java
@@ -0,0 +1,14 @@
+package D1;
+import java.util.Scanner;
+
+
+//신문의 헤드라인(2047-D1)
+public class Number2047 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		
+		System.out.println(str.toUpperCase());
+		
+	}
+}
